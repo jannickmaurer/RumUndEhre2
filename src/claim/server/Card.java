347832d9570a81107353e3d4e871faf4c751ca69
Muster <blackjack@eclipso.ch>
2021-04-12 17:47:08 +0200

    Card: Enum elements created to creat suit and rank of a card

--- src/claim/server/Card.java
+++ src/claim/server/Card.java
@@ -1,5 +1,62 @@
 package claim.server;
 
-public class Card {
 
+public class Card implements Comparable<Card>{
+	public enum Suit { Goblin, Dwarf, Undead, Double, Knight;
+        @Override
+        public String toString() {
+            String suit = "";
+            switch (this) {
+            case Goblin: suit = "goblin"; break;
+            case Dwarf:  suit = "dwarf";  break;
+            case Undead: suit = "undead"; break;
+            case Double: suit = "double"; break;
+            case Knight: suit = "knight"; break;
+            }
+            return suit;
+        }
+	};
+	
+    public enum Rank { Zero, One, Two, Three, Four, Five, Six, Seven, Eight, Nine;
+        @Override
+        public String toString() {
+            // Get ordinal value, which ranges from 0 to 9
+            int ordinal = this.ordinal();
+            return Integer.toString(ordinal);
+        }
+    }
+
+    private final Suit suit;
+    private final Rank rank;
+	
+    public Card(Suit suit, Rank rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+
+
+	public Suit getSuit() {
+        return suit;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+    
+    @Override
+    public String toString() {
+        return rank.toString() + suit.toString();
+    }
+	
+	
+	@Override
+	public int compareTo(Card o) {
+		if(this.getRank().compareTo(o.getRank()) > 0 ) {
+			return 1;
+		} else if(this.getRank().compareTo(o.getRank()) < 0) {
+				return -1;
+			} 
+		return 0;
+	}	
 }
