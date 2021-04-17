26542d66d861e63cf1dcae0f66047902aec6e88f
SamuelDavid <SamuelDavid@192.168.1.102>
2021-04-17 15:03:04 +0200

    GUI - GamePane Erweiterung, Card Label

--- src/claim/client/view/GamePane.java
+++ src/claim/client/view/GamePane.java
@@ -2,35 +2,104 @@
 
 package claim.client.view;
 
+import java.util.ArrayList;
+
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class GamePane extends BorderPane {
-	private Label lbArea1 = new Label("AREA1");
-	private Label lbArea2 = new Label("AREA2");
-	private Label lbArea3 = new Label("AREA3");
+	private HBox hboxMiddleCards = new HBox();
+	private CardLabel clCardsDeck = new CardLabel();
+	private CardLabel clNewFollowerDeck = new CardLabel();
+	
+	//Layouts für unterschiedliche Areas
+	public PlayerPane playerLayout = new PlayerPane();
+	public OtherPlayerPane otherPlayerLayout = new OtherPlayerPane();
+	
+	//Elemente aus Player Layout ansprechen
+	private Label lbName = playerLayout.getLbName();
+	private Label lbPoints = playerLayout.getLbPoints();
+	private Label lbPointsPlayer = playerLayout.getLbPointsPlayer();
+	private ArrayList cardLabels = playerLayout.getCardLabels();
+	
+	//Elemente aus Other Player Layout ansprechen
+	private Label lbOtherName = otherPlayerLayout.getLbName();
+	private Label lbOtherPoints = otherPlayerLayout.getLbPoints();
+	private Label lbOtherPointsPlayer = otherPlayerLayout.getLbPointsPlayer();
+	
 	private Button btLogout = new Button();
-	private VBox vbTest = new VBox();
+	private VBox vbPlayer = new VBox();
 	
 	//Konstruktor
-		public GamePane() {
-			vbTest.getChildren().addAll(lbArea3, btLogout);
+	public GamePane() {
+		
+		VBox vboxCardsDeck = new VBox();
+		Label lbCardsDeck = new Label("Cards Deck");
+		vboxCardsDeck.getChildren().addAll(clCardsDeck, lbCardsDeck);
+		
+		VBox vboxNewFollowerDeck = new VBox();
+		Label lbNewFollowerDeck = new Label("New Follower Deck");
+		vboxNewFollowerDeck.getChildren().addAll(clNewFollowerDeck, lbNewFollowerDeck);
+		
+		hboxMiddleCards.getChildren().addAll(vboxCardsDeck, vboxNewFollowerDeck);
+		hboxMiddleCards.setSpacing(10);
+		vbPlayer.getChildren().addAll(playerLayout, btLogout);
 			
-			this.setTop(lbArea1);
-			this.setCenter(lbArea2);
-			this.setBottom(vbTest);
-		}
+		this.setTop(otherPlayerLayout);
+		this.setCenter(hboxMiddleCards);
+		this.setBottom(vbPlayer);
+	}
 		
+	//Getter & Setter
+	public Button getBtLogout() {
+		return btLogout;
+	}
 
-		//Getter & Setter
-		public Button getBtLogout() {
-			return btLogout;
-		}
+	public void setBtLogout(Button btLogout) {
+		this.btLogout = btLogout;
+	}
 
-		public void setBtLogout(Button btLogout) {
-			this.btLogout = btLogout;
-		}
-		
+
+	public Label getLbName() {
+		return lbName;
+	}
+
+
+	public void setLbName(Label lbName) {
+		this.lbName = lbName;
+	}
+
+
+	public Label getLbPoints() {
+		return lbPoints;
+	}
+
+
+	public void setLbPoints(Label lbPoints) {
+		this.lbPoints = lbPoints;
+	}
+
+
+	public Label getLbPointsPlayer() {
+		return lbPointsPlayer;
+	}
+
+
+	public void setLbPointsPlayer(Label lbPointsPlayer) {
+		this.lbPointsPlayer = lbPointsPlayer;
+	}
+
+
+	public ArrayList getCardLabels() {
+		return cardLabels;
+	}
+
+
+	public void setCardLabels(ArrayList cardLabels) {
+		this.cardLabels = cardLabels;
+	}
+	
 }
