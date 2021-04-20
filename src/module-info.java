218fb3f1bc32f9ade0b9d865925be64891bd4967
Muster <blackjack@eclipso.ch>
2021-04-20 23:46:46 +0200

    No Plan

---
+++ src/module-info.java
@@ -0,0 +1,12 @@
+module RumUndEhre2 {
+	exports claim.client.controller;
+	exports claim.commons.messages;
+	exports claim.server;
+	exports claim.commons.messages.results;
+	exports claim.commons;
+	exports claim.client;
+	exports claim.client.view;
+	exports claim.client.model;
+
+	requires java.logging;
+}
